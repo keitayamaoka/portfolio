9268702a7fe8b3b0dae49ed2aba95bd787722eb8
keitayamaoka <62986434+keitayamaoka@users.noreply.github.com>
2020-04-09 17:06:32 +0900

    【ポートフォリオ実装】20200409

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,34 @@
 <template>
-   <div>
-     About
+   <div id="aboutSection">
+     <div class="aboutTitle">About Me</div>
+     <div class="aboutExplain">愛媛県生まれ、高知県育ち</div>
    </div>
 </template>
+
 <script>
 export default {
-    name: 'About'
+  name: 'About'
 }
 </script>
+
+<style scoped>
+#aboutSection{
+  background: #f5f5f5;
+  width: 100%;
+  height: auto;
+  text-align: center;
+  }
+
+  .aboutTitle{
+  padding: 20px;
+  color: #20879F;
+  font-family: Noto Sans, Bold;
+  font-size: 18pt;
+  }
+
+  .aboutExplain{
+  color: #707070;
+  font-family: Noto Sans, Bold;
+  font-size: 12pt;
+  }
+</style>
